f41ca340395b00be4769e1333d411ad6ec27cca2
220v-K <jwlee010222@gmail.com>
2023-06-09 21:46:57 +0900

    first commit

---
+++ Hey-Porori/Hey_PororiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hey_PororiApp.swift
+//  Hey-Porori
+//
+//  Created by 이재원 on 2023/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Hey_PororiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
