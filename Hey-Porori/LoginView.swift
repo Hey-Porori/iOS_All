f69a5ec26662c42a338cf7122c6f0f69f5ebd6fd
220v-K <jwlee010222@gmail.com>
2023-08-06 12:54:06 +0900

    #3 <feat>: Apple Login 버튼 초기화

---
+++ Hey-Porori/LoginView.swift
@@ -0,0 +1,39 @@
+//
+//  LoginView.swift
+//  Hey-Porori
+//
+//  Created by 이재원 on 2023/08/07.
+//
+
+import SwiftUI
+import AuthenticationServices
+
+struct LoginView: View {
+    var body: some View {
+        VStack {
+            SignInWithAppleButton { request in
+                request.requestedScopes = [.fullName, .email]
+            } onCompletion: { result in
+                switch result {
+                case .success(let authResults):
+                    print("Apple Login Successful")
+                    switch authResults.credential {
+                        case let appleIDCredential as ASAuthorizationAppleIDCredential:
+                           // 계정 정보 가져오기
+                            let userIdentifier = appleIDCredential.user
+                            let fullName = appleIDCredential.fullName
+                            let name =  (fullName?.familyName ?? "") + (fullName?.givenName ?? "")
+                            let email = appleIDCredential.email
+                            let identityToken = String(data: appleIDCredential.identityToken!, encoding: .utf8)
+                            let authorizationCode = String(data: appleIDCredential.authorizationCode!, encoding: .utf8)
+                    default:
+                        break
+                    }
+                case .failure(let error):
+                    print(error.localizedDescription)
+                    print("error")
+                }
+            }
+        }
+    }
+}
